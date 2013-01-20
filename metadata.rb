8bf16c9b71327d5fc757d86c7ae25ba172bc16a4
yudozen <yudozen@gmail.com>
2013-01-20 18:03:05 +0900

    mod

--- metadata.rb
+++ metadata.rb
@@ -1,5 +1,5 @@
-maintainer       "YOUR_COMPANY_NAME"
-maintainer_email "YOUR_EMAIL"
+maintainer       "yudozen"
+maintainer_email "yudozen@gmail.com"
 license          "All rights reserved"
 description      "Installs/Configures capybara-webkit"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
